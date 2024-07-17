5e2908eca80611f22785bd56365b5d9976e8a365
alishermamirov <alishermamirov452@gmail.com>
2024-07-17 16:29:30 +0500

    product and home screen added

---
+++ lib/widgets/product_grid_item.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:shopping_app/models/product.dart';
+
+class ProductGridItem extends StatelessWidget {
+  final Product product;
+  const ProductGridItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black.withOpacity(0.5),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          ),
+        ),
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+        ),
+      ),
+    );
+  }
+}
