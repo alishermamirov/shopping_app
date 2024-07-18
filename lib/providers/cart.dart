aaf2f716e93ff5a85520fed44cfbb62cf098e1c4
alishermamirov <alishermamirov452@gmail.com>
2024-07-18 11:43:46 +0500

    cart added

---
+++ lib/providers/cart.dart
@@ -0,0 +1,38 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:shopping_app/models/cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addToCart(String productId, String title, double price, String image) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (currentProduct) => CartItem(
+          id: currentProduct.id,
+          title: currentProduct.title,
+          quantity: currentProduct.quantity + 1,
+          price: currentProduct.price,
+          image: currentProduct.image,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: UniqueKey().toString(),
+          title: title,
+          quantity: 1,
+          price: price,
+          image: image,
+        ),
+      );
+    }
+  }
+}
